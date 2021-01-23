e72ff1078c04be27851a049d97fd188318486ea5
Rezan  Mohammad <rezan.moh83@gmail.com>
2021-01-23 01:57:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React template made with Parcel
+
+React + Redux + SASS template
+
+## Setup
+```yarn```
+
+## Start app
+```yarn start```
+
+## Build
+```yarn build```
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/index.scss";
+
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
